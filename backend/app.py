4140ff2d72a7a60948670e4f1b3c6736f18acbab
Royal <royal.21ai040@sode-edu.in>
2024-06-28 21:37:16 +0530

    remove extra files

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,8 @@ import boto3
 import requests
 import psycopg2
 import textwrap
+
+from groq import Groq
 from io import BytesIO
 from psycopg2 import pool
 from flask_cors import CORS
@@ -19,6 +21,7 @@ app = Flask(__name__)
 CORS(app)
 
 # CONFIG
+BUCKET = "demo"
 ALLOWED_EXTENSIONS = {"pas", "dfm", "cob", "cbl", "vb", "vbs"}
 DATABASE_URL = os.getenv("DATABASE_URL")
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
@@ -28,7 +31,9 @@ AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
 
 conn = psycopg2.connect(DATABASE_URL, sslmode="require")
 connection_pool = pool.SimpleConnectionPool(1, 10, DATABASE_URL, sslmode="require")
-client = Groq(api_key=os.getenv("MISTRAL_API_KEY"))
+client = Groq(
+    api_key=os.environ["MISTRAL_API_KEY"],
+)
 
 s3_client = boto3.client(
     "s3",
@@ -46,7 +51,7 @@ def upload_to_s3(filename, filecontent):
             Bucket=S3_BUCKET_NAME,
             Key=f"{filename}.json",
             Body=filecontent.encode("utf-8"),
-            ContentType="application/json"
+            ContentType="application/json",
         )
         return True
     except Exception as e:
@@ -56,12 +61,11 @@ def upload_to_s3(filename, filecontent):
 @app.route("/", methods=["GET"])
 def home():
     con_id = str(uuid.uuid4())
-    return "Running on Python with Postgres ", 200
+    return "Running on Python with Postgres", 200
 
 @app.route("/signup", methods=["POST"])
 def signup():
     data = request.get_json()
-
     email = data.get("email")
     password = data.get("password")
 
@@ -80,7 +84,12 @@ def signup():
     conn.commit()
     connection_pool.putconn(conn)
 
-    return {"status": 200, "email": email, "id": user_id, "message": "User created successfully"}, 200
+    return {
+        "status": 200,
+        "email": email,
+        "id": user_id,
+        "message": "User created successfully",
+    }, 200
 
 @app.route("/signin", methods=["POST"])
 def signin():
@@ -106,7 +115,12 @@ def signin():
             return {"status": 400, "error": "Invalid password"}, 200
     connection_pool.putconn(conn)
 
-    return {"status": 200, "email": email, "id": user_id, "message": "Signed in successfully"}, 200
+    return {
+        "status": 200,
+        "email": email,
+        "id": user_id,
+        "message": "Signed in successfully",
+    }, 200
 
 @app.route("/convert", methods=["POST"])
 def convert():
@@ -132,11 +146,11 @@ def convert():
                 {
                     "role": "user",
                     "content": f"""{code}
-                    covert the above {from_code} program to {to_code}, give the code first and then documentations
+                    convert the above {from_code} program to {to_code}, give the code first and then documentations
                     """,
                 }
             ],
-            model="mixtral-8x7b-32768",
+            model="llama3-8b-8192",
         )
 
         con_id = str(uuid.uuid4())
@@ -148,26 +162,34 @@ def convert():
             )
         conn.commit()
         connection_pool.putconn(conn)
+
         content = chat_completion.choices[0].message.content
+        print("Chat Completion Content:", content)  # Debugging line
+
         code_match = re.search(r"```(.*?)```", content, re.DOTALL)
-        codee = code_match.group(1).strip()
-        lines = codee.split('\n')
-        lines = lines[1:]
-        codee = '\n'.join(lines)
-        if upload_to_s3(filename=f"{con_id}-old", filecontent=code) and upload_to_s3(filename=f"{con_id}-new", filecontent=codee):
-            return {"code": codee, "id": con_id, "title": con_title}, 200
-        else:
-            return {"status": 400, "error": "File upload failed"}, 400
+        if code_match:
+            codee = code_match.group(1).strip()
+            lines = codee.split("\n")
+            lines = lines[1:]
+            codee = "\n".join(lines)
+            if upload_to_s3(
+                filename=f"{con_id}-old", filecontent=code
+            ) and upload_to_s3(filename=f"{con_id}-new", filecontent=codee):
+                return {"code": codee, "id": con_id, "title": con_title}, 200
+            else:
+                return {"status": 400, "error": "File upload failed"}, 400
 
     return {"status": 400, "error": "Invalid file extension"}, 400
 
 @app.route("/chatid/get", methods=["GET"])
 def get_chat_ids():
-    user_id = request.args.get('userid')
+    user_id = request.args.get("userid")
     conn = connection_pool.getconn()
     try:
         with conn.cursor() as cur:
-            cur.execute("SELECT con_id, title FROM history WHERE user_id = %s", (user_id,))
+            cur.execute(
+                "SELECT con_id,title FROM history WHERE user_id = %s", (user_id,)
+            )
             records = cur.fetchall()
         conn.commit()
     except Exception as e:
@@ -178,10 +200,13 @@ def get_chat_ids():
 
 @app.route("/docs/get", methods=["GET"])
 def get_docs():
-    chatid = request.args.get('chatid')
-    title = request.args.get('title')
-    chat_url = f"https://{S3_BUCKET_NAME}.s3.{AWS_DEFAULT_REGION}.amazonaws.com/{chatid}.json"
+    chatid = request.args.get("chatid")
+    title = request.args.get("title")
+    chat_url = f"https://s3.{AWS_DEFAULT_REGION}.amazonaws.com/{S3_BUCKET_NAME}/{chatid}.json"
     response = requests.get(chat_url)
+    if response.status_code != 200:
+        return {"status": 400, "error": "Failed to fetch the code from S3"}, 400
+
     code = response.text
     prompt = f"""
 You are given a code snippet below:
@@ -202,26 +227,29 @@ Make sure the html file is well-structured and easy to follow, using appropriate
 """
 
     chat_completion = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt,
-                }
-            ],
-            model="mixtral-8x7b-32768",
-        )
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="llama3-8b-8192",
+    )
     content = chat_completion.choices[0].message.content
     return content
 
 @app.route("/debug/get", methods=["GET"])
 def debug_code():
-    chatid = request.args.get('chatid')
-    title = request.args.get('title')
-    chat_url = f"https://{S3_BUCKET_NAME}.s3.{AWS_DEFAULT_REGION}.amazonaws.com/{chatid}.json"
+    chatid = request.args.get("chatid")
+    title = request.args.get("title")
+    chat_url = f"https://s3.{AWS_DEFAULT_REGION}.amazonaws.com/{S3_BUCKET_NAME}/{chatid}.json"
     response = requests.get(chat_url)
+    if response.status_code != 200:
+        return {"status": 400, "error": "Failed to fetch the code from S3"}, 400
+
     code = response.text
     prompt = f"""{code}
-    above code is converted {title}, debug the code if necessary and add comments regarding the changes you made. Also add proper heading and subheading.
+    above code is converted {title}, debug the code if necessary and add comments regarding the changes you made. Also add proper heading and subheading
     """
 
     try:
@@ -232,12 +260,12 @@ def debug_code():
                     "content": prompt,
                 }
             ],
-            model="mixtral-8x7b-32768",
-    )
+            model="llama3-8b-8192",
+        )
     except Exception as e:
         return f"An error occurred: {e} --- {prompt}"
     content = chat_completion.choices[0].message.content
     return content
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True, port=8181)
