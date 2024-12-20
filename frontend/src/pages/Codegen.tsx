9577bbc52a67ff2f99214364fe170c4e67942920
Royal <royal.21ai040@sode-edu.in>
2024-12-20 21:15:52 +0530

    add cpp to modern code list

--- frontend/src/pages/Codegen.tsx
+++ frontend/src/pages/Codegen.tsx
@@ -49,6 +49,8 @@ export default function Codegen() {
 
   function getFileExtension() {
     switch (dropInputs.to) {
+      case "c++":
+        return "cpp";
       case "java":
         return "java";
       case "python":
@@ -145,6 +147,7 @@ export default function Codegen() {
                 className="flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
               >
                 <option value="">Select target language</option>
+                <option value="C++">C++</option>
                 <option value="java">Java</option>
                 <option value="python">Python</option>
                 <option value="csharp">C#</option>
